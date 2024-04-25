0c6be6848936776b555351d6944ea66afbd4595e
SimoneCalzari <calzarisimone@gmail.com>
2024-04-25 22:50:11 +0200

    feat: pagina 404 personalizzata

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once __DIR__ . '/partials/boos_font.php' ?>
+    <title>404 NOT FOUND</title>
+</head>
+
+<body class="bg-danger">
+    <main class="vh-100 d-flex flex-column justify-content-center align-items-center">
+        <p class="fs-2"><i class="fa-solid fa-face-frown"></i> <i class="fa-solid fa-face-angry"></i> 404 ERROR PAGE NOT FOUND <i class="fa-solid fa-hand"></i> <i class="fa-solid fa-ban"></i></p>
+        <p class="fs-1">Stop typing random stuff in my website url <i class="fa-solid fa-exclamation"></i> <i class="fa-solid fa-exclamation"></i> <i class="fa-solid fa-exclamation"></i> </p>
+        <p class="fs-5">If you weren't and you feel lost I'm sorry I didn't mean to be so rude... </p>
+        <p>Here's your way back home <i class="fa-solid fa-shoe-prints"></i><i class="fa-solid fa-shoe-prints"></i><i class="fa-solid fa-shoe-prints"></i>
+            <a href="index.php" class="btn btn-dark ms-3 text-danger"><i class="fa-solid fa-house-fire me-1"></i> Home <i class="fa-solid fa-house-fire ms-1"></i> </a>
+        </p>
+    </main>
+</body>
+
+</html>
